8671be419760ae0d632f78836c081f78a917a0c6
Juan Veronez <juan.monteirov@gmail.com>
2021-09-20 17:59:46 -0300

    Transactions table created

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summury } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summury />
+      <TransactionsTable />
     </Container>
   );
 }
