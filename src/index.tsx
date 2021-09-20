f96f35be91cdd2bbba5f48b8f2830a67c20af1e6
Juan Veronez <juan.monteirov@gmail.com>
2021-09-20 19:21:01 -0300

    add axios and mirage to use fake api and create example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createServer } from 'miragejs';
+
 import { App } from './App';
+
+
+createServer({
+  routes() {
+    //nome inicial que vai ser procurado para as rotas (sendo assim será localhost:3000/api)
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [ 
+        {
+          id: 1,
+          title: 'Transaction 1'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
